e2d4c64dae306bc017478750d88875c9909a3ce0
Keijiro Takahashi <keijiro@gmail.com>
2023-11-05 18:08:42 +0900

    Shader implementation

--- Assets/Recolor.hlsl
+++ Assets/Recolor.hlsl
@@ -1,17 +1,36 @@
 void RecolorSamplePoints_float
-  (float2 UV, out float2 UV1, out float2 UV2, out float2 UV3)
+  (float2 UV, float Width, float Height,
+   out float2 UV1, out float2 UV2, out float2 UV3)
 {
-    UV1 = UV + 0.01;
+    UV1 = UV + float2(1 / Width, 1 / Height);
     UV2 = float2(UV.x, UV1.y);
     UV3 = float2(UV1.x, UV.y);
 }
 
 void RecolorMain_float
-  (float3 C0, float3 C1, float3 C2, float3 C3, out float3 Output)
+  (float3 C0, float3 C1, float3 C2, float3 C3,
+   float4 EdgeColor, float2 EdgeThreshold, float FillOpacity,
+   float4 ColorKey0, float4 ColorKey1, float4 ColorKey2, float4 ColorKey3,
+   float4 ColorKey4, float4 ColorKey5, float4 ColorKey6, float4 ColorKey7,
+   out float3 Output)
 {
     // Roberts cross operator
-    float3 g1 = C1 - C0.rgb;
+    float3 g1 = C1 - C0;
     float3 g2 = C3 - C2;
     float g = sqrt(dot(g1, g1) + dot(g2, g2)) * 10;
-    Output = g;
+
+    float3 fill = ColorKey0.rgb;
+    float lum = Luminance(C0.rgb);
+
+    fill = lerp(fill, ColorKey1.rgb, saturate((lum - ColorKey0.w) / (ColorKey1.w - ColorKey0.w)));
+    fill = lerp(fill, ColorKey2.rgb, saturate((lum - ColorKey1.w) / (ColorKey2.w - ColorKey1.w)));
+    fill = lerp(fill, ColorKey3.rgb, saturate((lum - ColorKey2.w) / (ColorKey3.w - ColorKey2.w)));
+    fill = lerp(fill, ColorKey4.rgb, saturate((lum - ColorKey3.w) / (ColorKey4.w - ColorKey3.w)));
+    fill = lerp(fill, ColorKey5.rgb, saturate((lum - ColorKey4.w) / (ColorKey5.w - ColorKey4.w)));
+    fill = lerp(fill, ColorKey6.rgb, saturate((lum - ColorKey5.w) / (ColorKey6.w - ColorKey5.w)));
+    fill = lerp(fill, ColorKey7.rgb, saturate((lum - ColorKey6.w) / (ColorKey7.w - ColorKey6.w)));
+
+    float edge = smoothstep(EdgeThreshold.x, EdgeThreshold.y, g);
+    float3 cb = lerp(C0, fill, FillOpacity);
+    Output = lerp(cb, EdgeColor.rgb, edge * EdgeColor.a);
 }
